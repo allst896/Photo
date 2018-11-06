213799391710db20ac3649c9e0ac981659e1b520
Amanda (Mandi) Howell <allst896@users.noreply.github.com>
2018-11-06 12:03:00 -0600

    Create email for default client - first try

--- PhotoEdit/MainWindow.xaml.cs
+++ PhotoEdit/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Net.Mail;
+using System.Reflection;
 
 namespace PhotoEdit
 {
@@ -127,10 +129,61 @@ namespace PhotoEdit
             try
             {
                 //open outlook with to, from, subject and attachment
+                //create an email
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("your@email.com", "MyCompany");
+                message.Subject = "Email subject goes here";
+                message.IsBodyHtml = true;
+                message.Body = "<html><head></head><body><font size=\"3\" color=\"#ff0000\">HTML formatted email body.</body></html>";
+
+                //add the attachments
+                message.Attachments.Add(new Attachment("PhotoEdit.pdb"));
+
+                //get the message as byte array
+                byte[] bin = getEmailAsEML(message);
+
+                //save byte array to file
+                string path = "C://Temp/mymessage.eml";
+                File.WriteAllBytes(path, bin);
+
+                //send the email to the client as eml
+                System.Diagnostics.Process.Start(path);
             }
             catch (Exception ex)
             {
 
+            }
+        }
+
+        public byte[] getEmailAsEML(MailMessage message)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var binaryWriter = new BinaryWriter(ms);
+                //Write the Unsent header to the file so the mail client knows this mail must be presented in "New message" mode
+                binaryWriter.Write(Encoding.UTF8.GetBytes("X-Unsent: 1" + Environment.NewLine));
+
+                var assembly = typeof(SmtpClient).Assembly;
+                var mailWriterType = assembly.GetType("System.Net.Mail.MailWriter");
+
+                // Get reflection info for MailWriter contructor
+                var mailWriterContructor = mailWriterType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(Stream) }, null);
+
+                // Construct MailWriter object with our FileStream
+                var mailWriter = mailWriterContructor.Invoke(new object[] { ms });
+
+                // Get reflection info for Send() method on MailMessage
+                var sendMethod = typeof(MailMessage).GetMethod("Send", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                sendMethod.Invoke(message, BindingFlags.Instance | BindingFlags.NonPublic, null, new object[] { mailWriter, true, true }, null);
+
+                // Finally get reflection info for Close() method on our MailWriter
+                var closeMethod = mailWriter.GetType().GetMethod("Close", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                // Call close method
+                closeMethod.Invoke(mailWriter, BindingFlags.Instance | BindingFlags.NonPublic, null, new object[] { }, null);
+
+                return ms.ToArray();
             }
         }
 
